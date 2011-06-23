a9753eace259c1a78168dcfd426388e39e08ef66
egghead <thomas.omans@gmail.com>
2011-06-23 21:06:43 +0000

    tweaks to weather.js to make it more readable and fix unhandled errors

--- plugins/weather.js
+++ plugins/weather.js
@@ -6,39 +6,48 @@
 
 module.exports = function(app) {
   var command, xml, parse, weather;
+
   command = app.util.command;
   parse = app.util.parse;
   xml = app.util.xml;
+
   weather = function(nous) {
     return command(nous, "weather", function(input) {
       var url, urisafe;
+
       urisafe = encodeURIComponent(input.msg);
       url = "http://www.google.com/ig/api?weather=" + urisafe;
+
       return parse(url, function(err, $, data) {
-        var results = {}, city, conditions, grab;
-        grab = ["condition", "temp_f", "temp_c", "humidity", "wind_condition"];
-        conditions = xml(data);
-        city = conditions.weather.forecast_information.city["@"]["data"]
-        for (var i = 0, len = grab.length; i < len; i++) {
-          results[grab[i]] = conditions.weather.current_conditions[grab[i]]["@"]["data"]
-        }
-        console.log(results);
-        if(results.condition) {
-          var response;
-          response = input.from + ": " + 
-                     city + ": " + 
-                     results.condition + ", " + 
-                     results.temp_f + "F/" +
-                     results.temp_c + "C " +
-                     results.humidity + ", " +
-                     results.wind_condition;
-          return nous.say(input.to, response);
-        } else {
-          return nous.say('Sorry we couldn\'t find any weather info for ' + input.msg);
-        }
+        var response;
+        try {
+          var results = {}, city, conditions, grab;
+
+          grab = ["condition", "temp_f", "temp_c", "humidity", "wind_condition"];
+          conditions = xml(data);
+          city = conditions.weather.forecast_information.city["@"]["data"];
+
+          for (var i = 0, len = grab.length; i < len; i++) {
+            results[grab[i]] = conditions.weather.current_conditions[grab[i]]["@"]["data"]
+          };
+
+          if(results.condition) {
+            response = input.from + ": " + 
+                       city + ": " + 
+                       results.condition + ", " + 
+                       results.temp_f + "F/" +
+                       results.temp_c + "C " +
+                       results.humidity + ", " +
+                       results.wind_condition;
+          };
+        } catch (err) {
+            response = "Sorry we couldn't find any weather info for " + input.msg;
+        };
+        return nous.say(input.to, response);
       });
     });
   };
+
   return {
     start: weather
   };
