d3c2f4bab6a854cd6d833e7cc9616c9fafc5bf1b
egghead <thomas.omans@gmail.com>
2011-06-23 18:05:43 +0000

    adding weather.js by joshmanders

---
+++ plugins/weather.js
@@ -0,0 +1,45 @@
+/* 
+ * weather.js, written by joshmanders
+ * adapted by eggsby
+ * pulls out xml data from google apis
+*/
+
+module.exports = function(app) {
+  var command, xml, parse, weather;
+  command = app.util.command;
+  parse = app.util.parse;
+  xml = app.util.xml;
+  weather = function(nous) {
+    return command(nous, "weather", function(input) {
+      var url, urisafe;
+      urisafe = encodeURIComponent(input.msg);
+      url = "http://www.google.com/ig/api?weather=" + urisafe;
+      return parse(url, function(err, $, data) {
+        var results = {}, city, conditions, grab;
+        grab = ["condition", "temp_f", "temp_c", "humidity", "wind_condition"];
+        conditions = xml(data);
+        city = conditions.weather.forecast_information.city["@"]["data"]
+        for (var i = 0, len = grab.length; i < len; i++) {
+          results[grab[i]] = conditions.weather.current_conditions[grab[i]]["@"]["data"]
+        }
+        console.log(results);
+        if(results.condition) {
+          var response;
+          response = input.from + ": " + 
+                     city + ": " + 
+                     results.condition + ", " + 
+                     results.temp_f + "F/" +
+                     results.temp_c + "C " +
+                     results.humidity + ", " +
+                     results.wind_condition;
+          return nous.say(input.to, response);
+        } else {
+          return nous.say('Sorry we couldn\'t find any weather info for ' + input.msg);
+        }
+      });
+    });
+  };
+  return {
+    start: weather
+  };
+};
